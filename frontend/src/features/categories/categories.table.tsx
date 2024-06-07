6ed0426e993677ba91ce8b93555843ce017466a4
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-06-07 12:13:49 -0400

    started table

--- frontend/src/features/categories/categories.table.tsx
+++ frontend/src/features/categories/categories.table.tsx
@@ -1,1 +1,12 @@
+"use client";
+import React from 'react';
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
 
+export default function CategoriesTable() {
+  return (
+    
+  )
+}
